c0329cc28a137238d5704e16bbbbda0d132eb3e4
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-11-25 18:46:59 +0100

    added global styles to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import { GlobalStyle } from "./GlobalStyles";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle/>
     <App />
     </ThemeProvider>
   </React.StrictMode>
