cf919e65c2b965c2279b3436e31c94e3843c825b
Krystian Krupiński <krupinskikrycha@gmail.com>
2025-01-19 16:38:06 +0100

    moved globalStyles from index.js to App to avoid having themeProvider in two places and to have a possibilty to change theme color

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import Skillset from "./common/Skillset";
 import ToLearn from "./common/ToLearn";
 import PortfolioSection from "./common/PorfolioSection";
 import Footer from "./common/Footer";
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./theme";
+import { GlobalStyle } from "./GlobalStyles";
 import { useState } from "react";
 
 function App() {
-  const [isDark, setIsDark] = useState("light")
+  const [isDark, setIsDark] = useState(false);
+
   const themeToggler = () => {
-    isDark === "light" ? setIsDark('dark') : setIsDark('light')
-  }
+    setIsDark((prevState) => !prevState);
+  };
 
   return (
-    <ThemeProvider theme={isDark === "light" ?  darkTheme : lightTheme}>
-    <Container>
-      <Header/>
-      <Section title={"My skillset includes 🛠️"} body={<Skillset />} />
-      <Section title={"What i want to learn next 🚀"} body={<ToLearn />} />
-      <PortfolioSection />
-      <Footer />
-    </Container>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <Container>
+        <Header onToggleTheme={themeToggler} />
+        <Section title={"My skillset includes 🛠️"} body={<Skillset />} />
+        <Section title={"What I want to learn next 🚀"} body={<ToLearn />} />
+        <PortfolioSection />
+        <Footer />
+      </Container>
     </ThemeProvider>
   );
 }
