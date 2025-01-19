7c072f2be6fd5e7d883a41bb78bc8ee318b28367
Krystian Krupiński <krupinskikrycha@gmail.com>
2025-01-19 12:57:07 +0100

    added styles for dark theme and some icons for later

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ import Skillset from "./common/Skillset";
 import ToLearn from "./common/ToLearn";
 import PortfolioSection from "./common/PorfolioSection";
 import Footer from "./common/Footer";
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from "./theme";
+import { useState } from "react";
 
 function App() {
+  const [isDark, setIsDark] = useState("light")
+  const themeToggler = () => {
+    isDark === "light" ? setIsDark('dark') : setIsDark('light')
+  }
+
   return (
+    <ThemeProvider theme={isDark === "light" ?  darkTheme : lightTheme}>
     <Container>
-      <Header />
+      <Header/>
       <Section title={"My skillset includes 🛠️"} body={<Skillset />} />
       <Section title={"What i want to learn next 🚀"} body={<ToLearn />} />
       <PortfolioSection />
       <Footer />
     </Container>
+    </ThemeProvider>
   );
 }
 
--- src/theme.js
+++ src/theme.js
@@ -18,3 +18,21 @@ export const theme = {
     tabletHorizontal: "1199px",
   },
 };
+
+export const darkTheme = {
+  body: "#252525",
+  text: "#FFFFFF",
+  borderShadow: "#363636",
+  iconColor: "#DADADA",
+  iconStroke: "#252525",
+};
+
+export const lightTheme = {
+  body: "#FBFBFE",
+  headersAndMail: "#252525",
+  text: "#6E7E91",
+  border: "#D1D5DA4D",
+  borderShadow: "#090A3308",
+  portfolioNameColor: "#0366D6",
+};
+
