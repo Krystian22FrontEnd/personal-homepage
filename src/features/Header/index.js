2ca8a938cc496bf2b3c5528d176bfe5eb38d9436
Krystian Krupiński <krupinskikrycha@gmail.com>
2025-01-28 12:07:39 +0100

    Cut theme button components and styles from Header component and moved to standalone component

---
+++ src/features/Header/index.js
@@ -0,0 +1,37 @@
+import {
+  HeaderContainer,
+  HeaderImage,
+  HeaderColumnWrapper,
+  HeaderParagraph,
+  HeaderH2,
+  HeaderDescription,
+  HeaderButton,
+  HeaderMessageIcon,
+} from "./styled";
+import Me from "../../images/Krystian.jpg";
+import email from "../../email";
+
+const Header = ({ themeButton }) => {
+  return (
+    <>
+      {themeButton}
+      <HeaderContainer>
+        <HeaderImage src={Me} alt="Krystian Krupiński" />
+        <HeaderColumnWrapper>
+          <HeaderParagraph>This is</HeaderParagraph>
+          <HeaderH2>Krystian Krupiński</HeaderH2>
+          <HeaderDescription>
+            👨🏻‍💻 "I'm a Frontend Developer who loves to work in React and is
+            currently looking for new job opportunities."
+          </HeaderDescription>
+          <HeaderButton href={`mailto:${email}`}>
+            <HeaderMessageIcon />
+            Hire me
+          </HeaderButton>
+        </HeaderColumnWrapper>
+      </HeaderContainer>
+    </>
+  );
+};
+
+export default Header;
